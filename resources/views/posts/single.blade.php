7068be2cb9be44d64440a93ebae4bb70de88598c
Mahdi Mahmoodian <mahmoodian.m1999@gmail.com>
2022-11-23 20:25:38 +0330

    [feat] add single post view

--- resources/views/posts/single.blade.php
+++ resources/views/posts/single.blade.php
@@ -1,2 +1,6 @@
-<h2 class = "ms-1">{{$post -> title}}</h2>
-<h6 class = "ms-1">{{$post -> text}}</h6>
+<h2 class = "ms-1">{{$post->title}}</h2>
+<h6 class = "ms-1">{{$post->text}}</h6>
+@foreach($post->comments as $comment)
+    <p>{{$comment->user->name}}:</p>
+    <p>{{$comment->comment}}</p>
+@endforeach
