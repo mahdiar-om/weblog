13e12719ca8b27bd3fbc9d2da4e933fd881da995
Mahdi Mahmoodian <mahmoodian.m1999@gmail.com>
2022-11-23 19:43:32 +0330

    [refactor] refactor login view to standard bootstrap convention

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -7,28 +7,35 @@
 <html lang="en">
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-     
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
     <link rel="stylesheet" type="text/css" href="{{ asset('css/signin.css') }}" >
-    
-    
+
+
 </head>
 <body>
-    <form  method="POST" action = {{route('authenticate')}}>
-        @csrf
+    <div class="container">
+        <div class="row">
+            <div class="col">
 
-        <div class="mb-3">
-            <label class="form-label">Email address</label>
-            <input type="email" class="form-control" name="email" placeholder="email@example.com">
-          </div>
-          <div class="mb-3">
-            <label class="form-label">Password</label>
-            <input type="password" class="form-control" name="password" placeholder="Password">
-          </div>
-        <button type="submit">Click here to login</button>
-    </form>
-    
-    <li><a href="login/sign_up">click to sign up</a></li>
+                <form  method="POST" action = {{route('authenticate')}}>
+                    @csrf
+
+                    <div class="mb-3">
+                        <label class="form-label">Email address</label>
+                        <input type="email" class="form-control" name="email" placeholder="email@example.com">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Password</label>
+                        <input type="password" class="form-control" name="password" placeholder="Password">
+                    </div>
+                    <button type="submit">Click here to login</button>
+                </form>
+
+                <li><a href="login/sign_up">click to sign up</a></li>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
