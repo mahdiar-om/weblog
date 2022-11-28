970ed05462bc2e924441e1a3f2eec0c77cec122e
mahdiar <mahdiar.moghaddami@gmail.com>
2022-11-28 06:36:41 -0800

    [create] view comment page

--- resources/views/posts/single.blade.php
+++ resources/views/posts/single.blade.php
@@ -1,6 +1,22 @@
+
+@extends('templates.app')
+@extends('templates.post')
+
+@section('posts')
 <h2 class = "ms-1">{{$post->title}}</h2>
-<h6 class = "ms-1">{{$post->text}}</h6>
-@foreach($post->comments as $comment)
-    <p>{{$comment->user->name}}:</p>
-    <p>{{$comment->comment}}</p>
-@endforeach
+<h6 class = "ms-1">{{$post->text}}</h6><br>
+        @foreach($post->comments as $comment)
+            <p>{{$comment->user->name}}:</p>
+            <div class="card mb-4">
+                <div class="card-body">
+                  <p>{{$comment->comment}}</p>
+    
+                  <div class="d-flex justify-content-between">
+                    <div class="d-flex flex-row align-items-center">
+                      <p class="small mb-0 ms-2">{{$comment->user->name}}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        @endforeach
+@endsection
