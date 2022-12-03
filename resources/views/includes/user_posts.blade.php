64f17c36fed0cdeb0121b2759cabd0164c52fc17
mahdiar <mahdiar.moghaddami@gmail.com>
2022-12-03 05:02:04 -0800

    [add] your posts view

---
+++ resources/views/includes/user_posts.blade.php
@@ -0,0 +1,24 @@
+
+      @extends('templates.post')
+          
+      @section('content')
+        <a href = "" class="btn btn-larg bg-success mt-2 mb-3">your Post</a><br>
+        @foreach ($posts as $post)
+          <div class="card mb-4">
+            <div class="card-body">
+              <p class="text-small ms-2">{{$post['text']}}</p>
+              <h3 class="ms-4 ">{{$post['title']}}</h3>
+              <div class="d-flex justify-content-between">
+                <div class="d-flex flex-row align-items-center">
+                  <p class="small mb-0 ms-2">{{$post['name']}}</p>
+                </div>
+                <div class="d-flex flex-row align-items-center">
+                  <i class="far fa-thumbs-up mx-2 fa-xs text-black" style="margin-top: -0.16rem;"></i>
+                    <li><a href= {{route('posts.comments' , ['id' => $post->id])}}  class="small text-muted mb-0">View all comment</a></li>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          @endforeach
+      @endsection
--- resources/views/posts/user_post.blade.php
+++ resources/views/posts/user_post.blade.php
@@ -1,45 +1,22 @@
+@extends('templates.app')
+    @auth
+        @section('nav_content')
+            @include('includes.navbar')
+        @endsection
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-<body>
-  
-</body>
-</html>
-<nav>
-  @extends('templates.app')
-</nav>
-      @extends('templates.post')
-          
-      @section('posts')
-        @foreach ($posts as $post)
-        <p>{{$post->user->name}}:</p>
-          <div class="card mb-4">
-            <div class="card-body">
-              <p class="text-small ms-2">{{$post['text']}}</p>
-              <h3 class="ms-4 ">{{$post['title']}}</h3>
-              <div class="d-flex justify-content-between">
-                <div class="d-flex flex-row align-items-center">
-                  <p class="small mb-0 ms-2">{{$post['name']}}</p>
-                </div>
-                <div class="d-flex flex-row align-items-center">
-                    <li><a href = '' class="small text-muted mb-0">update</a></li>
-                  <i class="far fa-thumbs-up mx-2 fa-xs text-black" style="margin-top: -0.16rem;"></i>
-                  <form action="{{route('posts.store')}}" method="POST">  
-                    @csrf 
-                    <input type="text" value="{{$post->id}}" name = "id" hidden>
-                    <li><a href=  ''  class="small text-muted mb-0">Verification comment</a></li>
-                  </form>
+        @section('state')
+            log out
+        @endsection
+    @endauth
+    
+    @section('state')
+            log in
+    @endsection
 
-                </div>
-              </div>
-            </div>
-          </div>
+        
 
-          @endforeach
-      @endsection
+    @section('content')
+        <div class="container">
+            @include('includes.user_posts')
+        </div>
+    @endsection
